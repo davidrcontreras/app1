328684034f2bd91a8e6549b55231059286f017c4
David Contreras <david.contreras222@gmail.com>
2025-05-23 21:06:02 -0300

    I mody the name of the app in the file test_app.py

---
+++ test_app.py
@@ -0,0 +1,7 @@
+from app import app 
+# On the top the first app reference to app name, and second app reference to the drectory name 
+def test_home():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Boquita" in response.data
